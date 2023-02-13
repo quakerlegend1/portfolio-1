0dbe538092996d181f8c70be88b63fc0e3c70660
Armen <82414784+quakerlegend1@users.noreply.github.com>
2023-02-13 19:50:30 +0300

    Add files via upload

---
+++ default_script.js
@@ -0,0 +1,65 @@
+const container = document.querySelector(".container");
+const elements = document.querySelector(".elements");
+const element = document.querySelectorAll(".element");
+let about_section = document.querySelector(".about");
+let img_stat = document.querySelector(".img-stat");
+let info = document.querySelector(".info");
+// document.onload=about_section.style.visibility="hidden";
+
+// function improveFighters() {
+//     for(let i=0;i<fighters.length;i++) {
+        
+//         fighters[i].firstChild.addEventListener("click",(e)=>{});
+       
+
+//         if(fighters[i].hasAttribute("data-jones")) {
+//             fighters[i].firstChild.addEventListener("click", (e)=> {about_section.style.visibility="visible";openCode(jon_text);img_stat.setAttribute("src","./images/jon_stat.png");info.innerHTML="";});
+//             fighters[i].addEventListener("mouseover",(e)=>{e.target.style.cssText="background-image:url('./images/Jones_improved.png');background-size:cover;background-color: gold;border-color:rgba(161, 147, 19, 0.8);"});
+//             fighters[i].addEventListener("mouseout",(e)=>{e.target.style.cssText=null;fighters[i].firstChild.style.cssText=null});
+//         }
+        
+//         else if(fighters[i].hasAttribute("data-conor")) {
+//             fighters[i].firstChild.addEventListener("click",(e)=>{about_section.style.visibility="visible";openCode(mac_text);img_stat.setAttribute("src","./images/conor_stat.png");info.innerHTML="";});
+//             fighters[i].addEventListener("mouseover",(e)=>{e.target.style.cssText="background-image:url('./images/conor_improved.png');background-size:cover;background-color: gold;border-color:rgba(161, 147, 19, 0.8);"});
+//             fighters[i].addEventListener("mouseout",(e)=>{e.target.style.cssText=null;fighters[i].firstChild.style.cssText=null});
+//         }
+//         else if(fighters[i].hasAttribute("data-israel")) {
+//             fighters[i].firstChild.addEventListener("click",(e)=>{about_section.style.visibility="visible";openCode(israel_text);img_stat.setAttribute("src","./images/israel_stat.png");info.innerHTML="";});
+//             fighters[i].addEventListener("mouseover",(e)=>{e.target.style.cssText="background-image:url('./images/israel_improved.png');background-size:cover;background-color: gold;border-color:rgba(161, 147, 19, 0.8);"});
+//             fighters[i].addEventListener("mouseout",(e)=>{e.target.style.cssText=null;fighters[i].firstChild.style.cssText=null});
+//         }
+//         else if(fighters[i].hasAttribute("data-petr")) {
+//             fighters[i].firstChild.addEventListener("click",(e)=>{about_section.style.visibility="visible";openCode(petr_text);img_stat.setAttribute("src","./images/petr_stat.png");info.innerHTML="";});
+//             fighters[i].addEventListener("mouseover",(e)=>{e.target.style.cssText="background-image:url('./images/petr_improved.png');background-size:cover;background-color: gold;border-color:rgba(161, 147, 19, 0.8);"});
+//             fighters[i].addEventListener("mouseout",(e)=>{e.target.style.cssText=null;fighters[i].firstChild.style.cssText=null});
+//         }
+        
+//     }
+
+// }
+// improveFighters();
+
+
+// fighters[0].querySelector("button").addEventListener("click",(e)=>
+// {document.body.style.backgroundColor=`rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`});Менять цвет фона на рандомный по нажатию на кнопку
+
+// function openCode(text_name) {
+//     var every = 0;
+    
+// function writeCode() {
+    
+//     info.textContent+=text_name[0][every];
+//     every++;
+    
+//     if(info.textContent.length >= text_name[0].length) {
+//         clearInterval(clearInt);
+        
+//     }
+// }
+// var clearInt =  setInterval(writeCode, 10);
+
+// };
+   
+
+
+
