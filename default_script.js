b0cf75afae7c6974c472823ecae646acb44f2d67
Armen <82414784+quakerlegend1@users.noreply.github.com>
2023-02-24 15:02:27 +0300

    Update default_script.js

--- default_script.js
+++ default_script.js
@@ -20,46 +20,6 @@ const ZTE_Nubia_Red_Magic_7 = [`Процессор: Qualcomm Snapdragon 8 Gen 1,
 
 burgerButton.addEventListener("click",()=> {burgerNav.style.display="block";})
 
-// document.onload=about_section.style.visibility="hidden";
-
-// function improveFighters() {
-//     for(let i=0;i<fighters.length;i++) {
-        
-//         fighters[i].firstChild.addEventListener("click",(e)=>{});
-       
-
-//         if(fighters[i].hasAttribute("data-jones")) {
-//             fighters[i].firstChild.addEventListener("click", (e)=> {about_section.style.visibility="visible";openCode(jon_text);img_stat.setAttribute("src","./images/jon_stat.png");info.innerHTML="";});
-//             fighters[i].addEventListener("mouseover",(e)=>{e.target.style.cssText="background-image:url('./images/Jones_improved.png');background-size:cover;background-color: gold;border-color:rgba(161, 147, 19, 0.8);"});
-//             fighters[i].addEventListener("mouseout",(e)=>{e.target.style.cssText=null;fighters[i].firstChild.style.cssText=null});
-//         }
-        
-//         else if(fighters[i].hasAttribute("data-conor")) {
-//             fighters[i].firstChild.addEventListener("click",(e)=>{about_section.style.visibility="visible";openCode(mac_text);img_stat.setAttribute("src","./images/conor_stat.png");info.innerHTML="";});
-//             fighters[i].addEventListener("mouseover",(e)=>{e.target.style.cssText="background-image:url('./images/conor_improved.png');background-size:cover;background-color: gold;border-color:rgba(161, 147, 19, 0.8);"});
-//             fighters[i].addEventListener("mouseout",(e)=>{e.target.style.cssText=null;fighters[i].firstChild.style.cssText=null});
-//         }
-//         else if(fighters[i].hasAttribute("data-israel")) {
-//             fighters[i].firstChild.addEventListener("click",(e)=>{about_section.style.visibility="visible";openCode(israel_text);img_stat.setAttribute("src","./images/israel_stat.png");info.innerHTML="";});
-//             fighters[i].addEventListener("mouseover",(e)=>{e.target.style.cssText="background-image:url('./images/israel_improved.png');background-size:cover;background-color: gold;border-color:rgba(161, 147, 19, 0.8);"});
-//             fighters[i].addEventListener("mouseout",(e)=>{e.target.style.cssText=null;fighters[i].firstChild.style.cssText=null});
-//         }
-//         else if(fighters[i].hasAttribute("data-petr")) {
-//             fighters[i].firstChild.addEventListener("click",(e)=>{about_section.style.visibility="visible";openCode(petr_text);img_stat.setAttribute("src","./images/petr_stat.png");info.innerHTML="";});
-//             fighters[i].addEventListener("mouseover",(e)=>{e.target.style.cssText="background-image:url('./images/petr_improved.png');background-size:cover;background-color: gold;border-color:rgba(161, 147, 19, 0.8);"});
-//             fighters[i].addEventListener("mouseout",(e)=>{e.target.style.cssText=null;fighters[i].firstChild.style.cssText=null});
-//         }
-        
-//     }
-
-// }
-// improveFighters();
-// fighters[0].querySelector("button").addEventListener("click",(e)=>
-// {document.body.style.backgroundColor=`rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`});Менять цвет фона на рандомный по нажатию на кнопку
-
-
-
-
 function writeCode(text_name) { 
         let increment = 0;
         let interval_ID = setInterval(()=>{
@@ -73,7 +33,6 @@ function writeCode(text_name) {
         },10)
         
 }
-
 
 for (let i = 0; i < element_Buttons.length; i++) {
         element_Buttons[i].addEventListener("click",(e)=>{about_section.style.display="block"})
